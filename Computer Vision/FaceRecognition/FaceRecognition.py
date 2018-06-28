cdd0d53302999ef2c7f29ae1bd97c59d7c04d005
Arun Rajan <33062637+ArunGARV@users.noreply.github.com>
2018-06-28 14:59:42 +0530

    Update FaceRecognition.py

--- Computer Vision/FaceRecognition/FaceRecognition.py
+++ Computer Vision/FaceRecognition/FaceRecognition.py
@@ -1,5 +1,9 @@
 '''
-Faces and Eyes are detected from the input images. 
+Faces and Eyes are detected from the input images.  
+Object Detection using Haar feature-based cascade classifiers
+is a machine learning based approach where a cascade function 
+is trained from a lot of positive and negative images. 
+It is then used to detect objects in other images.
 '''
 
 import numpy as np
